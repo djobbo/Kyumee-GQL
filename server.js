afa784899bcae4da8233f5b79ea37b0746aa2990
Djobbo Maïga <corehallabusiness@gmail.com>
2019-10-01 07:48:25 +0000

    more types

---
+++ server.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+const database = `mongodb+srv://Corehalla:${process.env.MATLAS_KEY}@corehalla-xtv6m.mongodb.net/kyumee?retryWrites=true&w=majority`;
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const server = new ApolloServer({ typeDefs, resolvers });
+
+        server.listen('31199').then(({ url }) => {
+            console.log(`🚀  Server ready at ${url}`);
+        });
+
+        const discordBot = require('./discord-bot');
+
+    })
+    .catch(err => console.log(err));
